769ffd51454693675ef13796affe49f3ba6a5610
yehang <yehang@sanduspace.cn>
2018-01-05 10:19:46 +0800

    github login facebook login self login

---
+++ src/main/java/com/divide2/dic/controller/LabelController.java
@@ -0,0 +1,14 @@
+package com.divide2.dic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * create by bvvy
+ */
+@Controller
+@RequestMapping("/v1/label")
+public class LabelController {
+
+}
--- src/test/java/com/divide2/dic/controller/DicControllerTest.java
+++ src/test/java/com/divide2/dic/controller/DicControllerTest.java
@@ -1,16 +1,21 @@
 package com.divide2.dic.controller;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -20,15 +25,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class DicControllerTest {
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/snippets");
+
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .build();
+    }
 
     @Test
     public void list() throws Exception {
-        this.mockMvc.perform(get("/v1/dic/list")).andDo(print()).andExpect(status().isOk())
-                .andDo(document("home"));
+        this.mockMvc.perform(get("/v1/dic/list").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("index"));
+
     }
+
+
 
 }
