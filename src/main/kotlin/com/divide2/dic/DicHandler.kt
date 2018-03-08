7b5e3410c361a0f5c8a25a2a786cf9c1c9ad09e3
bvvy <bvvy@qq.com>
2018-03-08 23:52:38 +0800

    change java => kotlin

--- src/main/kotlin/com/divide2/dic/DicHandler.kt
+++ src/main/kotlin/com/divide2/dic/DicHandler.kt
@@ -1,6 +1,7 @@
 package com.divide2.dic
 
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -11,33 +12,27 @@ import reactor.core.publisher.Mono
  * Created by bvvy on 2018/1/4.
  * com.divide2.dic.controller
  */
-@RestController
-@RequestMapping("/v1/dic")
-class DicController(private val dicRepository: DicRepository) {
 
-    @GetMapping("/all")
+@Component
+class DicHandler(private val dicRepository: DicRepository) {
+
     fun all(serverRequest: ServerRequest): Mono<ServerResponse> =
             ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(Flux.fromIterable(dicRepository.findAll()), Dic::class.java)
 
 
-    @GetMapping("{id}")
-    fun get(@PathVariable id: Int): Mono<Dic> = Mono.empty()
+    fun get(serverRequest: ServerRequest): Mono<ServerResponse> = ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(Flux.fromIterable(dicRepository.findAll()), Dic::class.java)
 
 
-    @DeleteMapping("{id}")
     fun delete(@PathVariable id: Int?): Mono<Dic> = Mono.empty()
 
-    @PostMapping
     fun add(@RequestBody dic: Dic) {
         dicRepository.save(dic)
     }
 
-    @PatchMapping
     fun update(@RequestBody dic: Dic) {
         dicRepository.save(dic)
     }
 
-    @GetMapping("/page")
     fun page(params: Map<String, String>) {
         //        return Responser.ok(dicRepository.page.of(1, 3)));
     }
